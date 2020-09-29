949eaf31886e8363dfed46638c63441f82354e89
DuyLuu90 <58050608+DuyLuu90@users.noreply.github.com>
2020-09-28 22:49:01 -0700

    change config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     API_ENDPOINT: (process.env.NODE_ENV==='production')
-                ? process.env.REACT_APP_API_BASE_URL || 'https://nameless-hamlet-52392.herokuapp.com/api'
+                ? process.env.REACT_APP_API_BASE_URL || 'http://nameless-hamlet-52392.herokuapp.com/api'
                 : 'http://localhost:8000/api',
 
     API_TOKEN: process.env.REACT_APP_API_TOKEN,
